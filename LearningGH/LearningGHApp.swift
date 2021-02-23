10facf72f00ceb1f66cb3871ef18b6b35f0dae5a
Sauceyp <72051789+Sauceyp@users.noreply.github.com>
2021-02-23 16:18:16 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Fredrik Preus Dovland on 23/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
